d2862088c4ed39aee63abb912f48f29a2ede4820
Zizoubrown <zizumoha2015@gmail.com>
2020-05-03 22:55:18 +0300

    add a method used to return element that has the id attribute with the specified value

---
+++ script.js
@@ -0,0 +1,8 @@
+function hp (){ 
+    let day = document.getElementById("day").value;
+    let dd = parseInt(day)
+    let month = document.getElementById("month").value;
+    let mm = parseInt(month)
+    let year = document.getElementById('year').value;
+    let yy = parseInt(year)
+}
