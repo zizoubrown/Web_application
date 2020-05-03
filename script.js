10a521f72590dba7d2688d889ddd6ef489798e5c
Zizoubrown <zizumoha2015@gmail.com>
2020-05-03 23:03:26 +0300

    add gender and akan variables

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ function hp (){
     let weekDay = new Date(yy + "/" + mm + "/" + dd);
     let output = document.getElementById("output")
     let finalResults = weekDay.getDay()
+
+    let male = document.getElementById('male');
+    let female = document.getElementById('female');
+    let maleNames=["kwasi","Kwadwo","kwabena","kwaku","yaw","kofi","kwame"];
+    let femaleNames=["Akosuo","Adwoa","Abenna","Akua","Yaa","Afua","Ama"];
+    let daysOfTheWeek=["Sunday", "Monday" , "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",]
